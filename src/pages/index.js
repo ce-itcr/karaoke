8fb803912b1a3b5151686aa6b6f29eff25730bdd
angelortizv <angelo2898@gmail.com>
2021-09-10 10:51:48 -0600

    [ADD] Complementary sections of the landing page are added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import Navbar from '../components/landing/Navbar';
 import Sidebar from '../components/landing/Sidebar';
 import HeroSection from '../components/landing/HeroSection';
-//import InfoSection from '../components/InfoSection';
-//import { homeObjOne, homeObjTwo, homeObjFour } from '../components/InfoSection/Data';
+import InfoSection from '../components/landing/InfoSection';
+import { homeObjOne, homeObjTwo, homeObjFour, homeObjThree } from '../components/landing/InfoSection/Data';
 //import Services from '../components/Services';
 import Footer from '../components/landing/Footer';
 
@@ -19,11 +19,12 @@ const Home = () => {
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
-      {/*<InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
-      <Services />
-      <InfoSection {...homeObjFour} />*/}
-        <Footer />
+      <InfoSection {...homeObjThree}/>
+      {/*<Services />*/}
+      <InfoSection {...homeObjFour} />
+      <Footer />
     </>
   )
 }
