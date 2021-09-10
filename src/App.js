8e31d188c1803973c74b489d5df538a66751e1cc
angelortizv <angelo2898@gmail.com>
2021-09-10 10:08:43 -0600

    [ADD] Footer, Navbar, Sidebar components are added for the landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+import Home from './pages';
+//import SigninPage from "./pages/Signin";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        {/*<Route path="/signin" component={SigninPage} exact />*/}
+      </Switch>
+    </Router>
   );
 }
 
